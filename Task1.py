259d70bf17e5d6169745cafe65cbe0f2e0fb18ee
Kate Kozachenko <kottya1995@ukr.net>
2024-02-08 23:33:06 +0100

    first commit

---
+++ Task1.py
@@ -0,0 +1,17 @@
+# функція, яка аналізує файл і повертає загальну та середню суму заробітної плати всіх розробників
+def total_salary(path):
+    salary_sum = 0
+    salary_average = 0
+    with open('Developers.txt', 'r') as doc:
+        lines = [el.strip() for el in doc.readlines()]
+        salaries = []
+        for line in lines:
+            line = line.split(',')
+            salaries.append(int(line[1]))
+    salary_sum = sum(salaries)
+    salary_average = salary_sum/len(salaries)
+    result = (salary_sum, salary_average)
+    return result
+
+path = 'Developers.txt'
+print(total_salary(path))
