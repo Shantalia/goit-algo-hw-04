9de93bc3a217b4560c4373e4c9fddb56bdd71196
Kate Kozachenko <kottya1995@ukr.net>
2024-02-10 23:50:42 +0100

    Task3_start

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,1 +1,19 @@
-from colorama import 
+from colorama import Fore, Back, Style
+from pathlib import Path
+import sys
+
+# pip install -r requirements.txt python Task3.py
+def script(path):
+
+
+    return
+
+
+#path = Path("C:/Users/Shantalia/Documents/Python_course/goit-algo-hw-04/Task3")
+directory = Path("./.venv")
+lst = sys.argv
+#if len(lst) <=2:
+for path in directory.iterdir():
+    print(path)
+#else: 
+#    print("Wrong input!")
