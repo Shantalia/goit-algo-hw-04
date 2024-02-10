049e46242379145f186b243dd9ea48e68eb74aa0
Kate Kozachenko <kottya1995@ukr.net>
2024-02-10 22:34:00 +0100

    Synchronisation

--- Task4/Task4_main.py
+++ Task4/Task4_main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from Contacts_processing import parse_input, add_contact, change_contact, show_phone, show_all
 
 def main():
@@ -51,57 +50,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-from Contacts_processing import parse_input, add_contact, change_contact, show_phone, show_all
-
-def main():
-    print("Welcome to the assistant bot!")
-    #contacts = {}
-
-    while True:
-        user_input = input("Enter a command: ")
-        try:
-            command, *args = parse_input(user_input)
-        except ValueError:
-            print("There is no command in line!")
-            break
-        except UnboundLocalError:
-            print("There is no command in line! Bye!")
-            break
-
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            try:
-                print(add_contact(args))
-            except ValueError:
-                print("Unvalid data!")
-        elif command == "change":
-            try:
-                print(change_contact(args))
-            except ValueError:
-                print("Unvalid data!")
-            except FileNotFoundError:
-                print('No such file or it damaged')
-        elif command == "phone":
-            try:
-                if len(args) > 1:
-                    print("Unvalid data")
-                else:
-                    print(show_phone(args))
-            except FileNotFoundError:
-                print('No such file or it damaged')
-        elif command == "all":
-            try:
-                print(show_all())
-            except FileNotFoundError:
-                print('No such file or it damaged')
-        else:
-            print("Invalid command.")
-
-if __name__ == "__main__":
-    main()
->>>>>>> 1ab531c12eec208b51771e12664643cdd26232ca
